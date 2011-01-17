4e0b6ed689e6f2eb140e7290bf3b3bf8b8cdede1
yyycatch@gmail.com <yyycatch@gmail.com@882cab8a-5835-f74a-486f-4d3248eaa91b>
2011-01-17 16:58:20 +0000

    pluginなどの実験用フォルダを追加

---
+++ practice/plugins_question/test-pluguin_install.bat
@@ -0,0 +1,8 @@
+rem デプロイ用スクリプト
+rem 2010-12-31
+
+copy inc_basic_questions.nako "C:\Documents and Settings\kachi\My Documents\SVN\_tsubaki-wiki\plugins\basics"
+copy inc_basic_questions.nako "C:\xampp\htdocs\tsubaki\plugins\basics"
+copy ext_basic_questions.nako "C:\Documents and Settings\kachi\My Documents\SVN\_tsubaki-wiki\plugins\basics"
+copy ext_basic_questions.nako "C:\xampp\htdocs\tsubaki\plugins\basics"
+
