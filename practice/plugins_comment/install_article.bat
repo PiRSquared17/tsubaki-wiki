9cddc203082a61c6b31058e4aca947cb7a6cbcfc
yyycatch@gmail.com <yyycatch@gmail.com@882cab8a-5835-f74a-486f-4d3248eaa91b>
2011-03-27 05:15:58 +0000

    //コメント記述が行頭時のみ、置換する

---
+++ practice/plugins_comment/install_article.bat
@@ -0,0 +1,8 @@
+rem articleデプロイ用スクリプト
+rem 2011-03-24
+
+copy inc_article.nako "C:\Documents and Settings\kachi\My Documents\SVN\_tsubaki-wiki\plugins\basics"
+copy inc_article.nako "C:\xampp\htdocs\tsubaki\plugins\basics"
+copy ext_article.nako "C:\Documents and Settings\kachi\My Documents\SVN\_tsubaki-wiki\plugins\basics"
+copy ext_article.nako "C:\xampp\htdocs\tsubaki\plugins\basics"
+
---
+++ practice/plugins_comment/install_comment_line.bat
@@ -0,0 +1,8 @@
+rem comment_lineデプロイ用スクリプト
+rem 2011-03-23
+
+copy inc_comment_line.nako "C:\Documents and Settings\kachi\My Documents\SVN\_tsubaki-wiki\plugins\basics"
+copy inc_comment_line.nako "C:\xampp\htdocs\tsubaki\plugins\basics"
+copy ext_comment_line.nako "C:\Documents and Settings\kachi\My Documents\SVN\_tsubaki-wiki\plugins\basics"
+copy ext_comment_line.nako "C:\xampp\htdocs\tsubaki\plugins\basics"
+
