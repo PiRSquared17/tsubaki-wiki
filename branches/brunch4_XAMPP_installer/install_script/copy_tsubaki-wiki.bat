c104f004a3166aef3e28697f19b5545da5e755f7
yyycatch@gmail.com <yyycatch@gmail.com@882cab8a-5835-f74a-486f-4d3248eaa91b>
2011-05-03 00:21:08 +0000

    必要ファイルの素材を自動収集。hdconfの自動プログラムを追加

---
+++ branches/brunch4_XAMPP_installer/install_script/copy_tsubaki-wiki.bat
@@ -0,0 +1,22 @@
+rem インストーラ作成用スクリプト - 椿ウヰキをコピー
+rem 2011-05-02：新規作成
+
+copy ..\tsubaki-wiki.nako input\tsubaki-wiki
+copy ..\tsubaki-config.nako input\tsubaki-wiki
+copy ..\main.nako input\tsubaki-wiki
+copy ..\lib.nako input\tsubaki-wiki
+copy ..\lib_api.nako input\tsubaki-wiki
+copy ..\lib_command.nako input\tsubaki-wiki
+copy ..\lib_wiki_tags.nako input\tsubaki-wiki
+copy ..\lib_html_generate.nako input\tsubaki-wiki
+copy ..\plugins_list*.nako input\tsubaki-wiki
+copy ..\plugins_call*.nako input\tsubaki-wiki
+copy ..\tsubaki-wiki.png input\tsubaki-wiki
+
+erase /q input\tsubaki-wiki\home\*.*
+xcopy ..\home\ input\tsubaki-wiki\home\ /s /e /c /h /r /k /y /v /f
+erase /q input\tsubaki-wiki\theme\*.*
+xcopy ..\theme\ input\tsubaki-wiki\theme\ /s /e /c /h /r /k /y /v /f
+erase /q input\tsubaki-wiki\plugins\*.*
+xcopy ..\plugins\ input\tsubaki-wiki\plugins\ /s /e /c /h /r /k /y /v /f 
+
